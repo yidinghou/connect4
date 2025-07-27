2db49dcb8aae3ca019f6276a404f0942805e3028
Yiding Hou <yidinghou@gmail.com>
2025-07-27 07:38:29 -0400

    tests: start test cases for check_win

---
+++ tests/board/test_check_win.py
@@ -0,0 +1,172 @@
+
+
+from connect4.board import check_win
+import numpy as np
+import pytest
+X=1
+O=-1
+
+
+#region No Win Tests
+@pytest.fixture
+def no_win_board_1():
+    return np.array([
+        [X, X, X, 0, X, X, X],
+        [X, X, X, 0, X, X, X],
+        [0, 0, 0, 0, 0, 0, 0],
+        [X, X, X, 0, X, X, X],
+        [X, X, X, 0, X, X, X],
+        [X, X, X, 0, X, X, X]
+    ], dtype=int)
+
+@pytest.fixture
+def no_win_full_board_1():
+    return np.array([
+        [O, O, O, X, O, O, O],
+        [X, X, X, O, X, X, X],
+        [O, O, O, X, O, O, O],
+        [X, X, X, O, X, X, X],
+        [O, O, O, X, O, O, O],
+        [X, X, X, O, X, X, X]
+    ], dtype=int)
+
+@pytest.fixture
+def no_win_full_board_2():
+    return np.array([
+        [O, X, O, X, O, X, O],
+        [X, O, O, O, X, O, X],
+        [X, O, X, O, X, O, X],
+        [X, O, X, O, X, X, X],
+        [O, X, O, X, O, X, O],
+        [X, O, X, O, O, O, X]
+    ], dtype=int)
+
+@pytest.fixture
+def no_win_board_2():
+    return np.array([
+        [0, X, X, X, 0, X, X],
+        [X, X, X, 0, X, X, X],
+        [X, X, 0, X, X, X, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [X, X, X, 0, X, X, X],
+        [X, X, X, 0, X, X, X]
+    ], dtype=int)
+
+
+@pytest.fixture
+def board_fixture(request):
+    """Meta-fixture that resolves to the actual board fixture"""
+    return request.getfixturevalue(request.param)
+
+@pytest.mark.parametrize("board_fixture", [
+    "no_win_board_1",
+    "no_win_board_2",
+    "no_win_full_board_1",
+    "no_win_full_board_2"
+], indirect=True)
+def test_boards_with_floating_pieces(board_fixture):
+    """Test that boards with floating pieces are correctly identified as invalid"""
+    result = check_win(board_fixture)
+    assert result is False
+
+#endregion
+
+#region Win Tests
+
+DIAG_WINS = [
+    # 1st row
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 0, 0, 0]]),
+
+    # 2nd row
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 1, 0],
+               [0, 0, 0, 0, 1, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    # 3rd row
+    np.array([[0, 0, 0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+
+    np.array([[0, 0, 0, 1, 0, 0, 0],
+           [0, 0, 1, 0, 0, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0]]),
+]
+
+@pytest.mark.parametrize("board", DIAG_WINS)
+def test_diagonal_wins(board):
+    """Test diagonal winning conditions"""
+    result = check_win(board)
+    assert result is True, f"Expected win for diagonal board:\n{board}"
+
+#endRegion
+
