95d9944c6b8d8d48d6a35ad3f1a05aed8944d48e
Yiding Hou <yidinghou@gmail.com>
2025-07-27 07:44:31 -0400

    docs: simplify comments

--- src/connect4/board.py
+++ src/connect4/board.py
@@ -13,11 +13,9 @@ def check_valid_board(board_arr: np.ndarray) -> bool:
         bool: True if the board is valid, False otherwise.
     """
 
-    # Check if sum board pieces add up to 0 or 1
     if np.sum(board_arr) not in [0, 1]:
         return False
     
-    # Check there are no floating pieces
     if not check_floating_pieces(board_arr):
         return False
 
@@ -32,17 +30,11 @@ def check_floating_pieces(board_arr: np.ndarray) -> bool:
     Returns:
         bool: True if the board state is valid (no floating zeros), False otherwise.
     """
-    # Slice containing all rows EXCEPT the last one (the "above" rows)
+
     rows_above = board_arr[:-1, :]
-
-    # Slice containing all rows EXCEPT the first one (the "current" rows)
     rows_below = board_arr[1:, :]
-
-    # A floating zero exists where a value in `rows_below` is 0 AND
-    # the corresponding value in `rows_above` is not 0.
     has_floating_zero = (rows_above != 0) & (rows_below == 0)
 
-    # The board is valid if no such condition exists anywhere.
     return not np.any(has_floating_zero)
 
 
