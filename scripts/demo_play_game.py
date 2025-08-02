4d27b8b0b3c6210baf9ec2f9e6efe61b82f06788
Yiding Hou <yidinghou@gmail.com>
2025-08-02 10:13:22 -0400

    add timing

--- scripts/demo_play_game.py
+++ scripts/demo_play_game.py
@@ -2,6 +2,8 @@ from connect4.mcts import MCTSTree
 from connect4 import board
 from connect4 import data_collector
 import numpy as np
+import time
+
 
 O = -1
 X = 1
@@ -17,9 +19,10 @@ board_arr = np.array([
 
 
 is_terminal = False
-N = 1000
+N = 2000
 player = 1
 while not is_terminal:
+    start = time.time()
     tree = MCTSTree(board_arr, player=player, iterations=N)
     # Run an MCTS step. Since the board is terminal, no expansion should occur.
     for i in range(N):
@@ -41,3 +44,5 @@ while not is_terminal:
         print(f"is terminal: {is_terminal}")
     
     player = player * -1
+    print(f"Time taken for MCTS step: {time.time() - start:.2f} seconds")
+    print("--------------------------------------------------")
